92a3968085f6f79ff61495190342bb5fa3f1da27
Zu <zulfizupy@gmail.com>
2025-07-31 08:28:41 +0300

    update: integrate MainNav component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
     return (
-        <section className="container mx-auto grid place-items-center">
-            Hello, World!
+        <section className="container mx-auto grid">
+            <h1 className="text-3xl md:text-4xl font-bold bg-gradient-to-r from-blue-900 via-red-800 to-blue-900 bg-clip-text text-transparent drop-shadow-lg">Dashboard</h1>
         </section>
     );
 }
