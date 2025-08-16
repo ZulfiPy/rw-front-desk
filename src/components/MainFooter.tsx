895c2196c7f890a6f401f89e4b76399115dec5f5
Zu <zulfizupy@gmail.com>
2025-08-16 08:52:19 +0300

    feat: replace and encode hardcoded data

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,10 +1,13 @@
 const MainFooter = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="border-t border-gray-200 mt-6 lg:mt-0">
+    <footer className="border-t border-gray-200 mt-6">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="flex justify-between items-center flex-col sm:flex-row">
           <p className="sm:text-sm text-xs">
-            © 2025 RW-Rent OÜ. All rights reserved. Developed by 1 Zulf OÜ
+            &copy; {currentYear} RW-Rent OÜ. All rights reserved. Developed by 1
+            Zulf OÜ
           </p>
           <div className="flex space-x-4 sm:text-sm text-xs">
             <a href="#" className="hover:text-gray-900">
